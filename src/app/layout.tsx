9768ba009f60fc9aa0ae5a6c795c16dbc1abbcbd
dev웅 <dev.lsw91@gmail.com>
2024-08-27 18:01:24 +0900

    SEO - Bing 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
     robots: {
         index: true,
         follow: true,
-        nocache: true,
         googleBot: {
             index: true,
             follow: false,
