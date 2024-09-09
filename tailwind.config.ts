5235e2d88355acd16cf5bc11b4afe7ff669fd60b
dev웅 <dev.lsw91@gmail.com>
2024-09-09 14:00:12 +0900

    admin 페이지 기반작업

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,25 +8,72 @@ const config = {
   plugins: [tailwindAnimate, shadcnPlugin],
   darkMode: "class",
   theme:{
-    extend:{
-      screens:{
-        // 기본 브레이크포인트
-        'xs': '480px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
+  	extend: {
+		screens:{
+			// 기본 브레이크포인트
+			'xs': '480px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
 
-        // max- 브레이크포인트
-        'max-xs': { 'max': '479px' },
-        'max-sm': { 'max': '639px' },
-        'max-md': { 'max': '767px' },
-        'max-lg': { 'max': '1023px' },
-        'max-xl': { 'max': '1279px' },
-        'max-2xl': { 'max': '1535px' },
-      }
-    }
+			// max- 브레이크포인트
+			'max-xs': { 'max': '479px' },
+			'max-sm': { 'max': '639px' },
+			'max-md': { 'max': '767px' },
+			'max-lg': { 'max': '1023px' },
+			'max-xl': { 'max': '1279px' },
+			'max-2xl': { 'max': '1535px' },
+		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   }
 } satisfies Config;
 
