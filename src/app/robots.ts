de786ab6d432517e2673efed599cedfd960f3bda
biz.lsw91 <biz.lsw91@gmail.com>
2024-09-10 19:28:49 +0900

    robots 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,8 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: "*",
-            disallow: "/admin",
+            allow: "/",
+            disallow: "/admin/",
         },
         sitemap: "https://www.gmpeace.co.kr/sitemap.xml",
     };
