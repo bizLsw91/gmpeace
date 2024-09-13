032cde4d4fe01471574587705ef9b05207b7e62e
dev웅 <dev.lsw91@gmail.com>
2024-09-14 01:38:42 +0900

    배포 에러 fix 중 테스트

--- src/app/(client)/api/queries/notice.ts
+++ src/app/(client)/api/queries/notice.ts
@@ -3,20 +3,20 @@
 import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import axios from "axios";
 
-export interface INotice {
-    id: number;
-    user_id: number;
-    view_count: number;
-    title: string;
-    content: string;
-    author: string;
-    attachments: { name:string,url:string }[]|null;
-    photos: string[]|null;
-    created_at: string;
-    updated_at: string;
-}
-
-export interface INoticeFormValue extends Omit<INotice, "id" | "user_id" | "view_count" | "author" | "createdAt" | "updatedAt"> {}
+// export interface INotice {
+//     id: number;
+//     user_id: number;
+//     view_count: number;
+//     title: string;
+//     content: string;
+//     author: string;
+//     attachments: { name:string,url:string }[]|null;
+//     photos: string[]|null;
+//     created_at: string;
+//     updated_at: string;
+// }
+//
+// export interface INoticeFormValue extends Omit<INotice, "id" | "user_id" | "view_count" | "author" | "createdAt" | "updatedAt"> {}
 
 export const useNotices = (currPage:number) => {
     return useQuery({
