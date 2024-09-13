bdad61b1d285c2705f95e27ae0970a38218a34ef
dev웅 <dev.lsw91@gmail.com>
2024-09-14 01:50:10 +0900

    배포 에러 fix 중 테스트

--- src/app/(client)/api/queries/notice.ts
+++ src/app/(client)/api/queries/notice.ts
@@ -1,61 +1,8 @@
-//클라이언트에서 사용하는 쿼리들만 모아놓기
-// import {INoticeFormValue} from "@/types/notice";
-import { useQuery} from "@tanstack/react-query";
+import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
-
-// export interface INotice {
-//     id: number;
-//     user_id: number;
-//     view_count: number;
-//     title: string;
-//     content: string;
-//     author: string;
-//     attachments: { name:string,url:string }[]|null;
-//     photos: string[]|null;
-//     created_at: string;
-//     updated_at: string;
-// }
-//
-// export interface INoticeFormValue extends Omit<INotice, "id" | "user_id" | "view_count" | "author" | "createdAt" | "updatedAt"> {}
 
 export const useNotices = (currPage:number) => {
     return useQuery({
         queryKey:['notices'],
         queryFn: ()=>axios.get(`/api/notices?page=${currPage||1}` )});
 };
-
-// export const useNotice = (param: number) => {
-//     return useQuery({
-//         queryKey:['notices', param],
-//         queryFn: ()=>axios.get( `/api/notices/${param}` )});
-// };
-// export const useCreateNotices = () => {
-//     const queryClient = useQueryClient();
-//     return useMutation( {
-//         mutationFn: (body:INoticeFormValue)=> axios.post(`/api/notices`,body),
-//         onSuccess: () => {
-//             // 공지사항 삭제 후 캐시를 무효화하여 최신 데이터를 가져옵니다.
-//             queryClient.invalidateQueries({
-//                 queryKey: ['notices'],
-//             });
-//         },
-//         onError: (error) => {
-//             console.error('Error creating notices:', error);
-//         },
-//     });
-// };
-// export const useDeleteNotices = () => {
-//     const queryClient = useQueryClient();
-//     return useMutation( {
-//         mutationFn: (ids:number[])=> axios.delete(`/api/notices`,{data:{ids}}),
-//         onSuccess: () => {
-//             // 공지사항 삭제 후 캐시를 무효화하여 최신 데이터를 가져옵니다.
-//             queryClient.invalidateQueries({
-//                 queryKey: ['notices'],
-//             });
-//         },
-//         onError: (error) => {
-//             console.error('Error deleting notices:', error);
-//         },
-//     });
-// };
