3caa03bf25c58cb946be2eac432216d6a382f229
dev웅 <dev.lsw91@gmail.com>
2024-09-14 01:24:23 +0900

    배포 에러 fix 중 테스트

--- src/app/(client)/notice/page.tsx
+++ src/app/(client)/notice/page.tsx
@@ -2,7 +2,9 @@
 
 import {useNotices} from "@/app/(client)/api/queries/notice";
 import {INotice} from "@/types/notice";
+import {useQuery} from "@tanstack/react-query";
 import {Table, Pagination, TableColumnsType } from 'antd';
+import axios from "axios";
 import moment from "moment";
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
@@ -11,7 +13,9 @@ import React from "react";
 
 const NoticesList = () => {
     const [currentPage, setCurrentPage] = useState(1);
-    const { data, isLoading, isError } = useNotices(currentPage);
+    const { data, isLoading, isError } = useQuery({
+        queryKey:['notices'],
+        queryFn: ()=>axios.get(`/api/notices?page=${currentPage||1}` )});
     const router = useRouter();
 
     if (isLoading) {
